307e33484a97d613afd3221774d4aaeea97a4687
Jemma Nelson <nelsonjs@uw.edu>
2014-07-18 14:28:32 -0700

    Add test for FastQC v0.11.2 parsing. All green!
    No code changes required to pass testing. May want to write helper functions
    to abstract over some changes, depending on how other projects use this
    parser.

--- test.py
+++ test.py
@@ -95,6 +95,77 @@ class TestOverrepresentedPass(unittest.TestCase):
     def test_total_overrepresented_sequences(self):
         self.assertEqual(self.parser.get_total_percent_overrepresented_sequences(), Decimal('0.0'))
 
+class TestVersion11Parsing(unittest.TestCase):
+    expected_results = {
+        'Per base sequence quality': PASS_RESULT,
+        'Per tile sequence quality': FAIL_RESULT,
+        'Per sequence quality scores': PASS_RESULT,
+        'Per base sequence content': FAIL_RESULT,
+        'Per sequence GC content': WARN_RESULT,
+        'Per base N content': PASS_RESULT,
+        #'Per base GC content': WARN_RESULT, #Not in v11
+        'Sequence Length Distribution': PASS_RESULT,
+        'Basic Statistics': PASS_RESULT,
+        'Sequence Duplication Levels': FAIL_RESULT,
+        'Overrepresented sequences': WARN_RESULT,
+        'Adapter Content': PASS_RESULT,
+        'Kmer Content': FAIL_RESULT,
+    }
+
+    def setUp(self):
+        self.file = "examples/rna_v11.txt"
+        self.parser = FastQCParser(filename = self.file)
+
+    def test_version(self):
+        self.assertEqual(self.parser.version, '0.11.2')
+
+    def test_modules_present(self):
+        self.assertItemsEqual(self.parser.modules.keys(), self.expected_results.keys())
+
+    def test_module_results(self):
+        for module in self.expected_results.keys():
+            self.assertEqual(self.expected_results[module], self.parser.module_results[module])
+
+    def test_basic_statistics(self):
+        table = self.parser.get_module_table('Basic Statistics')
+
+        self.assertIsNotNone(table)
+
+        self.assertEqual(table[0]['Measure'], 'Filename')
+        self.assertEqual(table[0]['Value'], 'DS28704B_ATGTCA_L002_R1_001.fastq.gz')
+
+        self.assertEqual(table[1]['Measure'], 'File type')
+        self.assertEqual(table[1]['Value'], 'Conventional base calls')
+
+        self.assertEqual(table[2]['Measure'], 'Encoding')
+        self.assertEqual(table[2]['Value'], 'Sanger / Illumina 1.9')
+
+        self.assertEqual(table[3]['Measure'], 'Total Sequences')
+        self.assertEqual(table[3]['Value'], '13608491')
+
+        self.assertEqual(table[4]['Measure'], 'Sequences flagged as poor quality')
+        self.assertEqual(table[4]['Value'], '1048299')
+
+        self.assertEqual(table[5]['Measure'], 'Sequence length')
+        self.assertEqual(table[5]['Value'], '76')
+
+        self.assertEqual(table[6]['Measure'], '%GC')
+        self.assertEqual(table[6]['Value'], '48')
+
+        headers = self.parser.get_module_table_headers('Basic Statistics')
+        self.assertItemsEqual(['Measure', 'Value'], headers)
+
+    def test_total_overrepresented_sequences(self):
+
+        self.assertEqual(self.parser.get_total_percent_overrepresented_sequences(), Decimal('6.32713796114499407'))
+
+    def test_info_values(self):
+
+        info_values = self.parser.get_module_info_values("Sequence Duplication Levels")
+
+        self.assertEqual(info_values['Total Deduplicated Percentage'], '42.86031810408766')
+
+
 if __name__ == '__main__':
     logging.basicConfig( stream=sys.stderr )
     logging.getLogger( "fastqc_parser" ).setLevel( logging.DEBUG )
